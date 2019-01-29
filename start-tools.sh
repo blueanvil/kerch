0b46ebeb28a9953f9a04c0c9a082ef47c2aa0e90
Cosmin Marginean <cos.marginean@gmail.com>
2019-01-29 20:54:03 +0000

    Cleanup

--- start-tools.sh
+++ start-tools.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-rm -rf etc/bluebash
-git clone https://github.com/blueanvil/bluebash etc/bluebash
-etc/bluebash/src/tools/elasticsearch.sh start etc/temp 6.6.0 blueanvil 9200 9300
+if [ -e etc/bluebash ]; then
+    git clone https://github.com/blueanvil/bluebash etc/bluebash
+fi
 
+etc/bluebash/src/tools/elasticsearch.sh start etc/temp 6.6.0 blueanvil 9200 9300
